cc7924175af4696ac5bb7c4c23b4906ce5ff9a2f
Gustavo dias <dscharraa@gmail.com>
2025-10-31 07:49:28 -0300

    adicionando logica do botao de resposta

---
+++ JSAccordion/script.js
@@ -0,0 +1,10 @@
+
+let buttonQuestion = document.getElementById('button-question');
+let answerText = document.getElementById('answer');
+
+
+function handleClick(){
+   answerText.innerHTML = "teste";
+}
+
+buttonQuestion.addEventListener(onclick,handleClick)
